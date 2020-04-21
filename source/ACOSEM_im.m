00b766ff79200ebba12d81a7b91fa85bc9a7331c
villekf <villewe@student.uef.fi>
2020-04-21 17:31:05 +0300

    Update ACOSEM
    Add support for implementation 4 and PSF reconstruction

--- source/ACOSEM_im.m
+++ source/ACOSEM_im.m
@@ -1,8 +1,10 @@
-function [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDelayed, is_transposed)
+function [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDelayed, is_transposed, varargin)
 %ACOSEM_IM Computes the Accelerated COSEM (ACOSEM) estimates
 %
-% Example:
+% Examples:
 %   [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDelayed, is_transposed)
+%   [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDelayed, is_transposed, options, Nx, Ny, Nz, gaussK)
+%   [im, C_aco] = ACOSEM_im(im, RHS, C_aco, D, osa_iter, h)
 % INPUTS:
 %   im = The current estimate
 %   A = The transpose of the (sparse) system matrix at current subset
@@ -15,8 +17,18 @@ function [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDela
 %   osa_iter = Current subset (sub-iteration)
 %   SinDelayed = Randoms and/or scatter correction data. Dimension must be
 %   either a scalar or a vector of same size as uu. If no scatter and/or
-%   randoms data is available, use zero. 
+%   randoms data is available, use zero.
 %   is_transposed = true if A matrix is the transpose of it, false if not
+%
+%   These values are needed only when doing PSF reconstruction:
+%	options = Needed for PSF reconstruction
+%   Nx = Image size in X-direction, for PSF
+%   Ny = Image size in Y-direction, for PSF
+%   Nz = Image size in Z-direction, for PSF
+%   gaussK = Gaussian kernel for PSF
+%
+%   This is for implementation 4:
+%   RHS = Backprojection computed by implementation 4
 %
 % OUTPUTS:
 %   im = The updated estimate
@@ -25,7 +37,7 @@ function [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDela
 %   See also COSEM_im, ECOSEM_im, COSEM_OSL
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright (C) 2019  Ville-Veikko Wettenhovi, Samuli Summala
+% Copyright (C) 2020 Ville-Veikko Wettenhovi, Samuli Summala
 %
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -40,18 +52,47 @@ function [im, C_aco] = ACOSEM_im(im, A, epps, uu, C_aco, D, h, osa_iter, SinDela
 % You should have received a copy of the GNU General Public License
 % along with this program. If not, see <https://www.gnu.org/licenses/>.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%C_aco(:,osa_iter) = full(sum(spdiags(uu./(A'*pz_acosem_apu + epps),0,size(A,2),size(A,2))*(bsxfun(@times,A',pz_acosem_apu'.^(1/h))))');
-if is_transposed
-    C_aco(:,osa_iter) = full(sum((spdiags(im.^(1/h),0,size(A,1),size(A,1))*A) * spdiags(uu./(A'*im + epps + SinDelayed),0,size(A,2),size(A,2)),2));
+% This section is for implementation 4
+if isempty(osa_iter) && isempty(is_transposed)
+    % Compute the scaled ACOSEM estimate
+    if isempty(uu)
+        im = (im)*(sum(epps)/(sum(A)));
+        C_aco = [];
+    else
+        % Compute the non-scaled ACOSEM estimate
+        epps(:,C_aco) = im.^(1/D) .* A;
+        im = (sum(epps,2) ./ uu).^D;
+        C_aco = epps;
+    end
 else
-    C_aco(:,osa_iter) = full(sum((spdiags(im.^(1/h),0,size(A,2),size(A,2))*A') * spdiags(uu./(A*im + epps + SinDelayed),0,size(A,1),size(A,1)),2));
+    % PSF blurring
+    if ~isempty(varargin) && ~isempty(varargin{1}) && varargin{1}.use_psf
+        im_apu = computeConvolution(im, varargin{1}, varargin{2}, varargin{3}, varargin{4}, varargin{5});
+    else
+        im_apu = im;
+    end
+    if is_transposed
+        FP = A' * im_apu + epps + SinDelayed;
+        % Backprojection
+        RHS = A * (uu ./ FP);
+    else
+        FP = A * im_apu + epps + SinDelayed;
+        RHS = A' * (uu ./ FP);
+    end
+    if ~isempty(varargin) && ~isempty(varargin{1}) && varargin{1}.use_psf
+        RHS = computeConvolution(RHS, varargin{1}, varargin{2}, varargin{3}, varargin{4}, varargin{5});
+    end
+    C_aco(:,osa_iter) = im.^(1/h) .* RHS;
+    im = (sum(C_aco,2)./D).^h;
+    if ~isempty(varargin) && ~isempty(varargin{1}) && varargin{1}.use_psf
+        im_apu = computeConvolution(im, varargin{1}, varargin{2}, varargin{3}, varargin{4}, varargin{5});
+    else
+        im_apu = im;
+    end
+    if is_transposed
+        im = (im)*(sum(uu)/(sum(A'*im_apu)));
+    else
+        im = (im)*(sum(uu)/(sum(A*im_apu)));
+    end
+    im(im < epps) = epps;
 end
-im = (sum(C_aco,2)./D).^h;
-% im = sum(C_aco,2);
-% disp(num2str(sum(A'*im)))
-if is_transposed
-    im = (im)*(sum(uu)/(sum(A'*im)));
-else
-    im = (im)*(sum(uu)/(sum(A*im)));
-end
-    im(im < 0) = epps;
