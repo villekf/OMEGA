5823386e7e7389bfa38c95962f4383e105f443dd
villekf <villewe@student.uef.fi>
2020-04-22 17:45:47 +0300

    Update for appliedCorrections

--- source/loadStructFromFile.m
+++ source/loadStructFromFile.m
@@ -1,3 +1,11 @@
-function result = loadStructFromFile(fileName, environmentName)
+function [result, varargout] = loadStructFromFile(fileName, environmentName, varargin)
 tmp = load(fileName, environmentName);
 result = tmp.(environmentName);
+if nargin > 2
+    try
+        tmp = load(fileName, varargin{1});
+        varargout{1} = tmp.(varargin{1});
+    catch
+        varargout{1} = [];
+    end
+end
