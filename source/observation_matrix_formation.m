dc62460ab6a71a5269dc4d88300566b4a53ea558
villekf <villewe@student.uef.fi>
2020-04-27 16:27:29 +0300

    Fixes and update for R2020a

--- source/observation_matrix_formation.m
+++ source/observation_matrix_formation.m
@@ -12,7 +12,7 @@ function A = observation_matrix_formation(options, varargin)
 % See also custom_prior_prepass
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright (C) 2019  Ville-Veikko Wettenhovi
+% Copyright (C) 2020 Ville-Veikko Wettenhovi
 %
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ function A = observation_matrix_formation(options, varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-if nargin > 2 && options.subsets > 1
+if nargin > 1 && options.subsets > 1
     osa_iter = varargin{1};
 else
     osa_iter = 1;
@@ -43,8 +43,13 @@ if ~options.use_raw_data
     end
 end
 
+
+iij = double(0:options.Nx);
+jji = double(0:options.Ny);
+kkj = double(0:options.Nz);
+
 if options.precompute_lor == false
-    if ~isfield(options, 'index') || length(options.index) == 1 || length(options.index{osa_iter}) == 1
+    if ~isfield(options, 'index') || length(options.index) == 1 || (iscell(options.index) && length(options.index{osa_iter}) == 1)
         if options.use_raw_data
             options.index = {uint32(1 : options.detectors ^2/2 + options.detectors/2)'};
         else
@@ -54,10 +59,11 @@ if options.precompute_lor == false
     if options.use_raw_data == false
         if options.projector_type == 1 || options.projector_type == 0
             if exist('projector_mex','file') == 3
-                [ lor, indices, alkiot] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, ...
-                    options.zmax, options.vaimennus, options.normalization, SinD, options.pituus(osa_iter), options.attenuation_correction, options.normalization_correction, ...
-                    options.randoms_correction, uint16(0), uint32(0), uint32(0), options.NSinos, uint16(0), options.pseudot, options.det_per_ring, options.verbose, ...
-                    options.use_raw_data, uint32(2), options.ind_size, options.block1, options.blocks, options.index{osa_iter}, uint32(options.projector_type));
+                [ lor, indices, alkiot] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, ...
+                    options.x, options.y, options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, options.zmax, options.vaimennus, ...
+                    options.normalization, SinD, options.pituus(osa_iter), options.attenuation_correction, options.normalization_correction, options.randoms_correction, ...
+                    options.global_correction_factor, uint16(0), uint32(0), uint32(0), options.NSinos, uint16(0), options.pseudot, options.det_per_ring, options.verbose, ...
+                    options.use_raw_data, uint32(2), options.ind_size, options.block1, options.blocks, options.index{osa_iter}, uint32(options.projector_type), iij, jji, kkj);
             else
                 % The below lines allow for pure MATLAB
                 % implemention, i.e. no MEX-files will be
@@ -67,18 +73,13 @@ if options.precompute_lor == false
                 % NOTE: The below method is not
                 % recommended since it is much slower
                 % method.
-                [ lor, indices, alkiot, discard] = improved_siddon_atten( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, options.yy, options.xx, ...
-                    options.NSinos, options.NSlices, options.vaimennus, options.index{osa_iter}, options.pituus(osa_iter), options.attenuation_correction);
+                [ lor, indices, alkiot, discard] = improved_siddon_atten( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, ...
+                    options.z_det, options.x, options.y, options.yy, options.xx, options.NSinos, options.NSlices, options.vaimennus, options.index{osa_iter}, ...
+                    options.pituus(osa_iter), options.attenuation_correction);
                 alkiot = cell2mat(alkiot);
                 indices = indices(discard);
                 indices = cell2mat(indices) - 1;
             end
-        elseif options.projector_type == 2
-            [ lor, indices, alkiot] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, ...
-                options.zmax, options.vaimennus, options.normalization, SinD, options.pituus(osa_iter), options.attenuation_correction, options.normalization_correction, ...
-                options.randoms_correction, uint16(0), uint32(0), uint32(0), options.NSinos, uint16(0), options.pseudot, options.det_per_ring, options.verbose, ...
-                options.use_raw_data, uint32(2), options.ind_size, options.block1, options.blocks, options.index{osa_iter}, uint32(options.projector_type), ...
-                options.tube_width_xy, options.x_center, options.y_center, options.z_center, options.tube_width_z);
         else
             error('Unsupported projector type')
         end
@@ -89,41 +90,40 @@ if options.precompute_lor == false
             L = options.LL;
         end
         if options.projector_type == 1
-            [ lor, indices, alkiot] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, ...
-                options.zmax, options.vaimennus, options.normalization, SinD, uint32(0), options.attenuation_correction, options.normalization_correction, ...
-                options.randoms_correction, uint16(0), uint32(0), uint32(0), options.NSinos, L, options.pseudot, options.det_per_ring, options.verbose, ...
-                options.use_raw_data, uint32(2), options.ind_size, options.block1, options.blocks, uint32(0), uint32(options.projector_type));
-        elseif options.projector_type == 2
-            [ lor, indices, alkiot] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, ...
-                options.zmax, options.vaimennus, options.normalization, SinD, uint32(0), options.attenuation_correction, options.normalization_correction, ...
-                options.randoms_correction, uint16(0), uint32(0), uint32(0), options.NSinos, L, options.pseudot, options.det_per_ring, options.verbose, ...
-                options.use_raw_data, uint32(2), options.ind_size, options.block1, options.blocks, uint32(0), uint32(options.projector_type), options.tube_width_xy, ...
-                options.x_center, options.y_center, options.z_center, options.tube_width_z);
+            [ lor, indices, alkiot] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, ...
+                options.y, options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, options.zmax, options.vaimennus, options.normalization, SinD, ...
+                uint32(0), options.attenuation_correction, options.normalization_correction, options.randoms_correction, options.global_correction_factor, uint16(0), uint32(0), ...
+                uint32(0), options.NSinos, L, options.pseudot, options.det_per_ring, options.verbose, options.use_raw_data, uint32(2), options.ind_size, options.block1, ...
+                options.blocks, uint32(0), uint32(options.projector_type), iij, jji, kkj);
         else
             error('Unsupported projector type')
         end
     end
-%     lor = reshape(lor,[],2);
     A_length = length(lor);
     if exist('OCTAVE_VERSION','builtin') == 0 && verLessThan('matlab','8.5')
-        lor = repeat_elem(int32(1:length(lor))',int32(lor));
+        lor = repeat_elem(uint32(1:length(lor))',uint32(lor));
     elseif exist('OCTAVE_VERSION','builtin') == 5
-        lor = repelem(int32(1:length(lor)),int32(lor));
+        lor = repelem(uint32(1:length(lor)),uint32(lor));
     else
-        lor = repelem(int32(1:length(lor)),int32(lor))';
+        lor = repelem(uint32(1:length(lor)),uint32(lor))';
     end
     
-    indices=indices + 1;
     if options.verbose
         tStart = tic;
     end
-    if options.use_fsparse && exist('fsparse','file') == 3
-        A = fsparse(lor,int32(indices),double(alkiot),[A_length double(options.N) length(alkiot)]);
+    if exist('OCTAVE_VERSION','builtin') == 0 && ~verLessThan('matlab','9.8')
+        indices = uint32(indices) + 1;
+        A = sparse(lor,indices,double(alkiot), A_length, double(options.N));
+    elseif options.use_fsparse && exist('fsparse','file') == 3
+        indices = int32(indices) + 1;
+        A = fsparse(int32(lor),(indices),double(alkiot),[A_length double(options.N) length(alkiot)]);
     elseif options.use_fsparse && exist('fsparse','file') == 0
         warning('options.fsparse set to true, but no FSparse mex-file found. Using regular sparse')
-        A = sparse(double(lor),double(indices),double(alkiot), A_length, double(options.N));
+        indices = double(indices) + 1;
+        A = sparse(double(lor),(indices),double(alkiot), A_length, double(options.N));
     else
-        A = sparse(double(lor),double(indices),double(alkiot), A_length, double(options.N));
+        indices = double(indices) + 1;
+        A = sparse(double(lor),(indices),double(alkiot), A_length, double(options.N));
     end
     clear indices alkiot lor
     if options.verbose
@@ -140,9 +140,9 @@ else
         xy_index_input = options.xy_index(options.pituus(osa_iter)+1:options.pituus(osa_iter + 1));
         z_index_input = options.z_index(options.pituus(osa_iter)+1:options.pituus(osa_iter + 1));
     end
-    if options.projector_type == 2 && options.subsets > 1
+    if (options.projector_type == 2 || options.projector_type == 3) && options.subsets > 1
         lor2 = [0; cumsum(uint64(options.lor_orth(options.pituus(osa_iter)+1:options.pituus(osa_iter + 1))))];
-    elseif options.projector_type == 2 && options.subsets == 1
+    elseif (options.projector_type == 2 || options.projector_type == 3) && options.subsets == 1
         lor2 = [0; uint64(options.lor_orth)];
     elseif options.projector_type == 1 && options.subsets == 1
         lor2 = [0; uint64(options.lor_a)];
@@ -150,17 +150,20 @@ else
         lor2 = [0; cumsum(uint64(options.lor_a(options.pituus(osa_iter)+1:options.pituus(osa_iter + 1))))];
     end
     if options.subsets > 1
-        [A, ~] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, options.zmax, options.vaimennus, ...
-            options.normalization, SinD, options.pituus(osa_iter + 1) - options.pituus(osa_iter), options.attenuation_correction, options.normalization_correction, options.randoms_correction, ...
-            options.lor_a(options.pituus(osa_iter)+1:options.pituus(osa_iter + 1)), xy_index_input, z_index_input, options.NSinos, L_input, options.pseudot, options.det_per_ring, options.verbose, ...
-            options.use_raw_data, uint32(0), lor2, options.summa(osa_iter), false, uint32(options.projector_type), options.tube_width_xy, options.x_center, ...
-            options.y_center, options.z_center, options.tube_width_z, int32(options.accuracy_factor));
+        [A, ~] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, ...
+            options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, options.zmax, options.vaimennus, options.normalization, SinD, ...
+            options.pituus(osa_iter + 1) - options.pituus(osa_iter), options.attenuation_correction, options.normalization_correction, options.randoms_correction, ...
+            options.global_correction_factor, options.lor_a(options.pituus(osa_iter)+1:options.pituus(osa_iter + 1)), xy_index_input, z_index_input, options.NSinos, ...
+            L_input, options.pseudot, options.det_per_ring, options.verbose, options.use_raw_data, uint32(0), lor2, options.summa(osa_iter), false, ...
+            uint32(options.projector_type), options.tube_width_xy, options.x_center, options.y_center, options.z_center, options.tube_width_z, int32(0), options.bmin, ...
+            options.bmax, options.Vmax, options.V);
     else
         [A, ~] = projector_mex( options.Ny, options.Nx, options.Nz, options.dx, options.dz, options.by, options.bx, options.bz, options.z_det, options.x, options.y, options.dy, options.yy, options.xx , options.NSinos, options.NSlices, options.size_x, options.zmax, options.vaimennus, ...
             options.normalization, SinD, options.pituus(osa_iter + 1) - options.pituus(osa_iter), options.attenuation_correction, options.normalization_correction, options.randoms_correction, ...
             options.lor_a, options.xy_index, options.z_index, options.NSinos, options.LL, options.pseudot, options.det_per_ring, options.verbose, ...
             options.use_raw_data, uint32(0), lor2, options.summa, false, uint32(options.projector_type), options.tube_width_xy, options.x_center, ...
-            options.y_center, options.z_center, options.tube_width_z, int32(options.accuracy_factor));
+            options.y_center, options.z_center, options.tube_width_z, int32(0), options.bmin, ...
+            options.bmax, options.Vmax, options.V);
     end
     clear lor2
 end
