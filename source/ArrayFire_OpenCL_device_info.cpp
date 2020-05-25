a7452f2b1432356602b73c06f94706fc8755697a
villekf <villewe@student.uef.fi>
2020-05-25 12:10:27 +0300

    Update ArrayFire_OpenCL_device_info.cpp

--- source/ArrayFire_OpenCL_device_info.cpp
+++ source/ArrayFire_OpenCL_device_info.cpp
@@ -1,14 +1,11 @@
 #include "arrayfire.h"
 #include "mex.h"
 
-
 void mexFunction(int nlhs, mxArray *plhs[],
 	int nrhs, const mxArray*prhs[])
 
 {
 	const char * c = af::infoString();
-
-	//mexPrintf("\n%s\n", c);
 
 	plhs[0] = mxCreateString(c);
 }
