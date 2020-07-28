58cf80822dd03b414d31709031995cfb4ea51269
villekf <villewe@student.uef.fi>
2020-07-28 10:32:35 +0300

    Small improvements

--- source/sinogram_coordinates_2D.m
+++ source/sinogram_coordinates_2D.m
@@ -56,19 +56,7 @@ end
 % Determine the sinogram indices for each of the LOR
 
 % Form the detector vector pair
-L = zeros(sum(1:det_w_pseudo),2,'int32');
-jh = int32(1);
-for kk = int32(1) : (det_w_pseudo)
-    if exist('OCTAVE_VERSION','builtin') == 0 && exist('repelem', 'builtin') == 0
-        L(jh:(jh + (det_w_pseudo) - kk),:) = [repeat_elem((kk), det_w_pseudo-(kk-1)), ((kk):det_w_pseudo)'];
-    elseif exist('OCTAVE_VERSION','builtin') == 5
-        L(jh:(jh + (det_w_pseudo) - kk),:) = [repelem((kk), det_w_pseudo-(kk-1)), ((kk):det_w_pseudo)'];
-    else
-        L(jh:(jh + (det_w_pseudo) - kk),:) = [repelem((kk), det_w_pseudo-(kk-1))', ((kk):det_w_pseudo)'];
-    end
-    jh = jh + (det_w_pseudo) -kk + 1;
-end
-L(L(:,1) == 0,:) = [];
+L = formDetectorIndices(det_w_pseudo);
 
 L = L - 1;
 
