1e825987e11a0f20e8395b9cd81b47000e6c1940
villekf <villewe@student.uef.fi>
2020-08-05 17:02:04 +0300

    Create sinograms on the fly

--- source/inveon_list2matlab.cpp
+++ source/inveon_list2matlab.cpp
@@ -2,7 +2,7 @@
 * Loads the Siemens Inveon 48-bit list-mode data and stores the detector
 * indices of prompts and delays at specific time steps.
 *
-* Copyright (C) 2019 Ville-Veikko Wettenhovi
+* Copyright (C) 2020 Ville-Veikko Wettenhovi
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -26,12 +26,78 @@
 #include <algorithm>
 
 FILE *streami;
+#define DET_PER_RING 320
+#define RINGS 80
+
+void saveSinogram(uint16_t L1, uint16_t L2, uint16_t* Sino, const uint32_t Ndist, const uint32_t Nang, const uint32_t ring_difference, const uint32_t span, 
+	const uint64_t sinoSize, const uint32_t* seg, const int32_t nDistSide, const int tPoint = 0) {
+	int32_t ring_pos1 = L1 % DET_PER_RING;
+	int32_t ring_pos2 = L2 % DET_PER_RING;
+	int32_t ring_number1 = L1 / DET_PER_RING;
+	int32_t ring_number2 = L2 / DET_PER_RING;
+	const int32_t xa = std::max(ring_pos1, ring_pos2);
+	const int32_t ya = std::min(ring_pos1, ring_pos2);
+	int32_t j = ((xa + ya + DET_PER_RING / 2 + 1) % DET_PER_RING) / 2;
+	const int32_t b = j + DET_PER_RING / 2;
+	int32_t i = std::abs(xa - ya - DET_PER_RING / 2);
+	const bool ind = ya < j || b < xa;
+	if (ind)
+		i = -i;
+	const bool swap = (j * 2) < -i || i <= ((j - DET_PER_RING / 2) * 2);
+	bool accepted_lors;
+	if (Ndist % 2U == 0)
+		accepted_lors = (i <= (static_cast<int32_t>(Ndist) / 2 + std::min(0, nDistSide)) && i >= (-static_cast<int32_t>(Ndist) / 2 + std::max(0, nDistSide)));
+	else
+		accepted_lors = (i <= static_cast<int32_t>(Ndist) / 2 && i >= (-static_cast<int32_t>(Ndist) / 2));
+	accepted_lors = accepted_lors && (std::abs(ring_number1 - ring_number2) <= ring_difference);
+	int32_t sinoIndex = 0;
+	if (accepted_lors) {
+		j = j / (DET_PER_RING / 2 / Nang);
+		if (swap) {
+			const int32_t ring_number3 = ring_number1;
+			ring_number1 = ring_number2;
+			ring_number2 = ring_number3;
+		}
+		i = i + Ndist / 2 - std::max(0, nDistSide);
+		const bool swappi = ring_pos2 > ring_pos1;
+		if (swappi) {
+			const int32_t ring_number3 = ring_number1;
+			ring_number1 = ring_number2;
+			ring_number2 = ring_number3;
+		}
+		if (span <= 1) {
+			sinoIndex = ring_number2 * RINGS + ring_number1;
+		}
+		else {
+			const int32_t erotus = ring_number1 - ring_number2;
+			const int32_t summa = ring_number1 + ring_number2;
+			if (std::abs(erotus) <= span / 2) {
+				sinoIndex = summa;
+			}
+			else {
+				sinoIndex = ((std::abs(erotus) + (span / 2)) / span);
+				if (erotus < 0) {
+					sinoIndex = (summa - ((span / 2) * (sinoIndex * 2 - 1) + sinoIndex)) + static_cast<uint32_t>(seg[(sinoIndex - 1) * 2]);
+				}
+				else {
+					sinoIndex = (summa - ((span / 2) * (sinoIndex * 2 - 1) + sinoIndex)) + static_cast<uint32_t>(seg[(sinoIndex - 1) * 2 + 1]);
+				}
+			}
+		}
+		const uint64_t indeksi = static_cast<uint64_t>(i) + static_cast<uint64_t>(j) * static_cast<uint64_t>(Ndist) +
+			static_cast<uint64_t>(sinoIndex) * static_cast<uint64_t>(Ndist) * static_cast<uint64_t>(Nang) + sinoSize * tPoint;
+		Sino[indeksi]++;
+	}
+	else
+		return;
+}
 
 void histogram(uint16_t * LL1, uint16_t * LL2, uint32_t * tpoints, char **argv, const double vali, const double alku, const double loppu, const size_t outsize2,
-	const uint32_t detectors, const size_t pituus, const bool randoms_correction, uint16_t *DD1, uint16_t *DD2, uint8_t *TOF)
+	const uint32_t detectors, const size_t pituus, const bool randoms_correction, uint16_t *DD1, uint16_t *DD2, uint16_t *Sino, uint16_t* SinoD, const bool saveRawData, 
+	const uint32_t Ndist, const uint32_t Nang, const uint32_t ringDifference, const uint32_t span, const uint64_t sinoSize, const uint32_t* seg, const int32_t nDistSide)
 {
 
-	static long long int i, i1 = -1;//__int64
+	static uint64_t i;
 
 	static char *in_file;
 
@@ -60,6 +126,7 @@ void histogram(uint16_t * LL1, uint16_t * LL2, uint32_t * tpoints, char **argv, 
 	uint32_t L1;    //detector 1
 	uint32_t L2;    //detector 2
 	int mscount = 0;
+	int tPoint = 0;
 	int ll = -1;
 	double aika = alku + vali;
 	bool begin = false;
@@ -68,8 +135,6 @@ void histogram(uint16_t * LL1, uint16_t * LL2, uint32_t * tpoints, char **argv, 
 
 	mexPrintf("file opened \n");
 	while ((i = fread(&ew1, sizeof(qb) * 3, 1, streami)) != 0 && ms <= loppu) {
-
-		i1++;
 
 		tag = ((ew1 >> (43)) & 1);
 
@@ -89,12 +154,22 @@ void histogram(uint16_t * LL1, uint16_t * LL2, uint32_t * tpoints, char **argv, 
 					L2 = ew1 & 0x1ffff;
 					if (L1 >= detectors || L2 >= detectors)
 						continue;
-					if (outsize2 == 1) {
-						LL1[L1*detectors + L2] = LL1[L1*detectors + L2] + static_cast<uint16_t>(1);
-					}
-					else {
-						LL1[ll] = static_cast<uint16_t>(L1 + 1);
-						LL2[ll] = static_cast<uint16_t>(L2 + 1);
+					saveSinogram(L1, L2, Sino, Ndist, Nang, ringDifference, span, sinoSize, seg, nDistSide, tPoint);
+					if (saveRawData) {
+						if (L2 > L1) {
+							const uint32_t L3 = L1;
+							L1 = L2;
+							L2 = L3;
+						}
+						if (saveRawData) {
+							if (outsize2 == 1) {
+								LL1[L1 * detectors + L2] = LL1[L1 * detectors + L2] + static_cast<uint16_t>(1);
+							}
+							else {
+								LL1[ll] = static_cast<uint16_t>(L1 + 1);
+								LL2[ll] = static_cast<uint16_t>(L2 + 1);
+							}
+						}
 					}
 				}
 				else if (randoms_correction && prompt == 0) {
@@ -102,12 +177,22 @@ void histogram(uint16_t * LL1, uint16_t * LL2, uint32_t * tpoints, char **argv, 
 					L2 = ew1 & 0x1ffff;
 					if (L1 >= detectors || L2 >= detectors)
 						continue;
-					if (outsize2 == 1) {
-						DD1[L1*detectors + L2] = DD1[L1*detectors + L2] + static_cast<uint16_t>(1);
-					}
-					else {
-						DD1[ll] = static_cast<uint16_t>(L1 + 1);
-						DD2[ll] = static_cast<uint16_t>(L2 + 1);
+					saveSinogram(L1, L2, SinoD, Ndist, Nang, ringDifference, span, sinoSize, seg, nDistSide, tPoint);
+					if (saveRawData) {
+						if (L2 > L1) {
+							const uint32_t L3 = L1;
+							L1 = L2;
+							L2 = L3;
+						}
+						if (saveRawData) {
+							if (outsize2 == 1) {
+								DD1[L1 * detectors + L2] = DD1[L1 * detectors + L2] + static_cast<uint16_t>(1);
+							}
+							else {
+								DD1[ll] = static_cast<uint16_t>(L1 + 1);
+								DD2[ll] = static_cast<uint16_t>(L2 + 1);
+							}
+						}
 					}
 				}
 			}	
@@ -119,6 +204,7 @@ void histogram(uint16_t * LL1, uint16_t * LL2, uint32_t * tpoints, char **argv, 
 				if (ms >= aika) {
 					tpoints[mscount] = ll;
 					mscount++;
+					tPoint++;
 					aika += (vali);
 				}
 			}
@@ -140,11 +226,11 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
 	/* Check for proper number of arguments */
 
-	if (nrhs != 7) {
+	if (nrhs != 16) {
 		mexErrMsgIdAndTxt("MATLAB:list2matlab_aivi:invalidNumInputs",
-			"Seven input arguments required.");
+			"16 input arguments required.");
 	}
-	else if (nlhs > 6) {
+	else if (nlhs > 7) {
 		mexErrMsgIdAndTxt("MATLAB:list2matlab_aivi:maxlhs",
 			"Too many output arguments.");
 	}
@@ -155,6 +241,15 @@ void mexFunction(int nlhs, mxArray *plhs[],
 	size_t pituus = (size_t)mxGetScalar(prhs[4]);
 	uint32_t detectors = (uint32_t)mxGetScalar(prhs[5]);
 	bool randoms_correction = (bool)mxGetScalar(prhs[6]);
+	uint64_t sinoSize = (uint64_t)mxGetScalar(prhs[7]);
+	bool saveRawData = (bool)mxGetScalar(prhs[8]);
+	uint32_t Ndist = (uint32_t)mxGetScalar(prhs[9]);
+	uint32_t Nang = (uint32_t)mxGetScalar(prhs[10]);
+	uint32_t ringDifference = (uint32_t)mxGetScalar(prhs[11]);
+	uint32_t span = (uint32_t)mxGetScalar(prhs[12]);
+	uint32_t* seg = (uint32_t*)mxGetData(prhs[13]);
+	uint64_t NT = (uint64_t)mxGetScalar(prhs[14]);
+	int32_t nDistSide = (int32_t)mxGetScalar(prhs[15]);
 	//double energy_low1 = (double)mxGetScalar(prhs[7]);
 	//double energy_low2 = (double)mxGetScalar(prhs[8]);
 	//double energy_high1 = (double)mxGetScalar(prhs[9]);
@@ -163,41 +258,54 @@ void mexFunction(int nlhs, mxArray *plhs[],
 	//double energy_normal2 = (double)mxGetScalar(prhs[12]);
 	size_t outsize2 = (loppu - alku) / vali;
 
-	if (outsize2 == 1) {
-		plhs[0] = mxCreateNumericMatrix(detectors, detectors, mxUINT16_CLASS, mxREAL);
-		plhs[1] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
-		if (randoms_correction) {
-			plhs[3] = mxCreateNumericMatrix(detectors, detectors, mxUINT16_CLASS, mxREAL);
-			plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+	if (saveRawData) {
+		if (outsize2 == 1) {
+			plhs[0] = mxCreateNumericMatrix(detectors, detectors, mxUINT16_CLASS, mxREAL);
+			plhs[1] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+			if (randoms_correction) {
+				plhs[3] = mxCreateNumericMatrix(detectors, detectors, mxUINT16_CLASS, mxREAL);
+				plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+			}
+			else {
+				plhs[3] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+				plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+			}
 		}
 		else {
-			plhs[3] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
-			plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+			plhs[0] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
+			plhs[1] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
+			if (randoms_correction) {
+				plhs[3] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
+				plhs[4] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
+			}
+			else {
+				plhs[3] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+				plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+			}
 		}
 	}
 	else {
-		plhs[0] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
-		plhs[1] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
-		if (randoms_correction) {
-			plhs[3] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
-			plhs[4] = mxCreateNumericMatrix(pituus, 1, mxUINT16_CLASS, mxREAL);
-		}
-		else {
-			plhs[3] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
-			plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
-		}
+		plhs[0] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+		plhs[1] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+		plhs[3] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
+		plhs[4] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
 	}
 	plhs[2] = mxCreateNumericMatrix(outsize2 + 2, 1, mxUINT32_CLASS, mxREAL);
-	plhs[5] = mxCreateNumericMatrix(pituus, 1, mxUINT8_CLASS, mxREAL);
+	plhs[5] = mxCreateNumericMatrix(sinoSize * NT, 1, mxUINT16_CLASS, mxREAL);
+	if (randoms_correction)
+		plhs[6] = mxCreateNumericMatrix(sinoSize, 1, mxUINT16_CLASS, mxREAL);
+	else
+		plhs[6] = mxCreateNumericMatrix(1, 1, mxUINT16_CLASS, mxREAL);
 
 
 	/* Assign pointers to the various parameters */
-	uint16_t * LL1 = (uint16_t*)mxGetData(plhs[0]);
-	uint16_t * LL2 = (uint16_t*)mxGetData(plhs[1]);
-	uint32_t * tpoints = (uint32_t*)mxGetData(plhs[2]);
-	uint16_t * DD1 = (uint16_t*)mxGetData(plhs[3]);
-	uint16_t * DD2 = (uint16_t*)mxGetData(plhs[4]);
-	uint8_t * TOF = (uint8_t*)mxGetData(plhs[5]);
+	uint16_t* LL1 = (uint16_t*)mxGetData(plhs[0]);
+	uint16_t* LL2 = (uint16_t*)mxGetData(plhs[1]);
+	uint32_t* tpoints = (uint32_t*)mxGetData(plhs[2]);
+	uint16_t* DD1 = (uint16_t*)mxGetData(plhs[3]);
+	uint16_t* DD2 = (uint16_t*)mxGetData(plhs[4]);
+	uint16_t* Sino = (uint16_t*)mxGetData(plhs[5]);
+	uint16_t* SinoD = (uint16_t*)mxGetData(plhs[6]);
 
 	// Count inputs and check for char type
 	int argc = 0;
@@ -239,7 +347,8 @@ void mexFunction(int nlhs, mxArray *plhs[],
 		}
 	}
 
-	histogram(LL1, LL2, tpoints, argv, vali, alku, loppu, outsize2, detectors, pituus, randoms_correction, DD1, DD2, TOF);
+	histogram(LL1, LL2, tpoints, argv, vali, alku, loppu, outsize2, detectors, pituus, randoms_correction, DD1, DD2, Sino, SinoD, saveRawData, 
+		Ndist, Nang, ringDifference, span, sinoSize, seg, nDistSide);
 	return;
 
 }
