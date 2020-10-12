3ab755c8f3abac7fde3c6577e16f7040824eb566
villekf <villewe@student.uef.fi>
2020-10-12 14:23:15 +0300

    TOF updates

--- source/interpolateSinog.m
+++ source/interpolateSinog.m
@@ -4,6 +4,23 @@ function SinM = interpolateSinog(SinM,sampling, Ndist, partitions, sampling_inte
 %   interpolation method can be controlled with
 %   sampling_interpolation_method.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (C) 2020 Ville-Veikko Wettenhovi
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <https://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 if iscell(SinM)
     SinM_uus = cell(size(SinM));
     for hh = 1 : partitions
@@ -11,7 +28,7 @@ if iscell(SinM)
         for uu = 1 : size(SinM{hh},4)
             for kk = 1 : size(SinM{hh},3)
                 for ll = 1 : size(SinM{hh},2)
-                    SinM_uus{hh}(:,ll,kk,uu) = single(interp1(1:sampling:Ndist*sampling+1, double([SinM{hh}(:,ll,kk);SinM{hh}(end - 1,ll,kk)]), ...
+                    SinM_uus{hh}(:,ll,kk,uu) = single(interp1(1:sampling:Ndist*sampling+1, double([SinM{hh}(:,ll,kk,uu);SinM{hh}(end - 1,ll,kk,uu)]), ...
                         1:Ndist*sampling, sampling_interpolation_method));
                 end
             end
@@ -23,7 +40,7 @@ else
     for uu = 1 : size(SinM,4)
         for kk = 1 : size(SinM,3)
             for ll = 1 : size(SinM,2)
-                SinM_uus(:,ll,kk,uu) = interp1(1:sampling:Ndist*sampling+1, double([SinM(:,ll,kk);SinM(end - 1,ll,kk)]), ...
+                SinM_uus(:,ll,kk,uu) = interp1(1:sampling:Ndist*sampling+1, double([SinM(:,ll,kk,uu);SinM(end - 1,ll,kk,uu)]), ...
                     1:Ndist*sampling, sampling_interpolation_method);
             end
         end
