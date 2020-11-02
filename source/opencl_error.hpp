fcf1904ebb353210a461bab00115fe454f04a5e7
villekf <villewe@student.uef.fi>
2020-11-02 17:46:30 +0200

    Backwards compatibility for OpenCL for OpenCL 1.2 devices

--- source/opencl_error.hpp
+++ source/opencl_error.hpp
@@ -20,6 +20,7 @@
 //#define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
 //#if defined(__APPLE__) || defined(__MACOSX)
 //#include <OpenCL/cl.h>
 //#else
