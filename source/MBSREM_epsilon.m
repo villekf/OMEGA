5df0ff5af893d370c39d2bf63289b0e0afb6b749
villekf <villewe@student.uef.fi>
2021-04-19 16:32:31 +0300

    CT support

--- source/MBSREM_epsilon.m
+++ source/MBSREM_epsilon.m
@@ -1,14 +1,15 @@
-function epsilon_mramla = MBSREM_epsilon(Sino, epps, randoms_correction, randoms, D)
+function epsilon_mramla = MBSREM_epsilon(Sino, epps, randoms_correction, randoms, D, varargin)
 %MBSREM_epsilon Computes the epsilon value for MBSREM/MRAMLA
 %   INPUTS:
 %   Sino = Measurements at current time-step
-%   epps = Small constant to prevent division by zero
+%   epps = Small constant to prevent division by zero (unused)
 %   randoms_correction = Is randoms and/or scatter correction included
 %   randoms = Number of randoms and/or scattered coincidences
 %   D = Sum of the system matrix (measurements)
+%   CT = Whether the CT likelihood is used
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright (C) 2019  Ville-Veikko Wettenhovi
+% Copyright (C) 2021 Ville-Veikko Wettenhovi
 %
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -24,38 +25,62 @@ function epsilon_mramla = MBSREM_epsilon(Sino, epps, randoms_correction, randoms
 % along with this program. If not, see <https://www.gnu.org/licenses/>.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % I = find(Sino);
-if randoms_correction == 1
-    P_Sino = double(Sino(Sino > 0 & randoms == 0));
-    apu = D + double(randoms);
-    apu = sum(double(Sino) .* log(apu) - apu);
+if ~isempty(varargin) && ~isempty(varargin{1})
+    CT = varargin{1};
 else
-    P_Sino = double(Sino(Sino > 0));
-    apu = sum(D .* log(D) - D);
+    CT = false;
 end
-% hk_summa = zeros(nnz(I),1);
-% lhk = 1;
-% hk_summa = zeros(size(Sino));
-% hk_summa(Sino > 0) = double(Sino(Sino > 0)) .* log(double(Sino(Sino > 0))) - double(Sino(Sino > 0));
-hk_summa = double(Sino) .* log(double(Sino)) - double(Sino);
-hk_summa(isnan(hk_summa)) = 0;
-% hk_summa = double(P_Sino) .* log(P_Sino) - double(P_Sino);
-if randoms_correction == 1
-    hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa(Sino > 0 & randoms == 0));
-%     hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa);
+if CT
+    if randoms_correction == 1
+        P_Sino = double(Sino(Sino > 0 & randoms == 0));
+        apu = D + double(randoms);
+        apu = sum(-apu - double(Sino) ./ exp(apu));
+    else
+        P_Sino = double(Sino(Sino > 0));
+        apu = sum(-D - double(Sino) ./ exp(D));
+    end
+    hk_summa = -double(Sino) - double(Sino) ./ exp(double(Sino));
+    hk_summa(isnan(hk_summa)) = 0;
+    if randoms_correction == 1
+        hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa(Sino > 0 & randoms == 0));
+    else
+        hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa(Sino > 0));
+    end
+    epsilon_mramla = min(min([1./P_Sino';log((apu - hk_summa)./double(1./P_Sino))']));
 else
-    hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa(Sino > 0));
-%     hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa);
+    if randoms_correction == 1
+        P_Sino = double(Sino(Sino > 0 & randoms == 0));
+        apu = D + double(randoms);
+        apu = sum(double(Sino) .* log(apu) - apu);
+    else
+        P_Sino = double(Sino(Sino > 0));
+        apu = sum(double(Sino) .* log(D) - D);
+    end
+    % hk_summa = zeros(nnz(I),1);
+    % lhk = 1;
+    % hk_summa = zeros(size(Sino));
+    % hk_summa(Sino > 0) = double(Sino(Sino > 0)) .* log(double(Sino(Sino > 0))) - double(Sino(Sino > 0));
+    hk_summa = double(Sino) .* log(double(Sino)) - double(Sino);
+    hk_summa(isnan(hk_summa)) = 0;
+    % hk_summa = double(P_Sino) .* log(P_Sino) - double(P_Sino);
+    if randoms_correction == 1
+        hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa(Sino > 0 & randoms == 0));
+        %     hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa);
+    else
+        hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa(Sino > 0));
+        %     hk_summa = bsxfun(@minus, sum(hk_summa), hk_summa);
+    end
+    % apu = sum(P_Sino .* log(P_Sino) - P_Sino);
+    % apu = length(P_Sino);
+    % apu = sum(P_Sino .* log(1) - 1);
+    % for hkl = I
+    %     apu = I ~= hkl;
+    %     hk_summa(lhk) = sum(double(Sino(I(apu))).*log(double(Sino(I(apu)))) - double(Sino(I(apu))));
+    %     lhk = lhk + 1;
+    % end
+    epsilon_mramla = min(min([P_Sino';exp((apu - hk_summa)./double(P_Sino))']));
 end
-% apu = sum(P_Sino .* log(P_Sino) - P_Sino);
-% apu = length(P_Sino);
-% apu = sum(P_Sino .* log(1) - 1);
-% for hkl = I
-%     apu = I ~= hkl;
-%     hk_summa(lhk) = sum(double(Sino(I(apu))).*log(double(Sino(I(apu)))) - double(Sino(I(apu))));
-%     lhk = lhk + 1;
-% end
-epsilon_mramla = min(min([P_Sino';exp((apu - hk_summa)./double(P_Sino))']));
 if epsilon_mramla <= 0
-    epsilon_mramla = min(P_Sino);
+    epsilon_mramla = epps;
 end
 
