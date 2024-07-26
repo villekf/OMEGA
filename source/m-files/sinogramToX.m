821fa86c2c5a9ffc996485282f76af97d0d2ce96
saarlemo <45739791+saarlemo@users.noreply.github.com>
2024-07-26 16:24:27 +0300

    Gamma camera indexing fix

--- source/m-files/sinogramToX.m
+++ source/m-files/sinogramToX.m
@@ -12,21 +12,21 @@ function returnList = sinogramToX(angles, radii, nx, ny, crxy)
     end
 
     nIter = numel(angles);
-    returnList = zeros(6, nIter * nx * ny);
+    returnList = zeros(6, nIter * ny * nx);
     
-    panelXmin = -crxy * (nx - 1) / 2;
+    panelXmin = -crxy * (ny - 1) / 2;
     panelXmax = -panelXmin;
-    panelYmin = -crxy * (ny - 1) / 2;
+    panelYmin = -crxy * (nx - 1) / 2;
     panelYmax = -panelYmin;
     
     % Detector x points
-    x = zeros(ny, nx);
+    x = zeros(nx, ny);
     
     % Detector y points
-    y = repmat(linspace(panelYmin, panelYmax, ny)', [1, nx]);
+    y = repmat(linspace(panelYmin, panelYmax, nx)', [1, ny]);
     
     % Detector z points
-    z = repmat(linspace(panelXmin, panelXmax, nx), [ny, 1]);
+    z = repmat(linspace(panelXmin, panelXmax, ny), [nx, 1]);
     
     % Rotate and move
     idxCounter = 1;
@@ -36,8 +36,8 @@ function returnList = sinogramToX(angles, radii, nx, ny, crxy)
         
         nVec = rr*[cosd(ang); sind(ang)]; % Panel normal
         R = [cosd(ang) -sind(ang); sind(ang) cosd(ang)]; % Rotation matrix
-        for ii = 1:nx
-            for jj = 1:ny
+        for ii = 1:ny
+            for jj = 1:nx
                 detXY = R * [x(jj, ii); y(jj, ii)] + nVec;
                 detZ = z(jj, ii);
     
