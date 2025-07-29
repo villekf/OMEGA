21039f4a7b0767142b2f8180349a2ca871c1e8a2
villekf <villewe@student.uef.fi>
2025-07-29 18:19:59 +0300

    Fixed increased sinogram sampling in Python

--- source/Python/omegatomo/util/sampling.py
+++ source/Python/omegatomo/util/sampling.py
@@ -15,25 +15,24 @@ def increaseSampling(options, x=None, y=None, interpolate_sinogram=False):
         x, y = detectorCoordinates(options)
         x, y = sinogramCoordinates2D(options, x, y)
 
-    # Reshape coordinates
     xx1 = x[:, 0].reshape((options.Ndist, options.Nang), order='F')
     xx2 = x[:, 1].reshape((options.Ndist, options.Nang), order='F')
     yy1 = y[:, 0].reshape((options.Ndist, options.Nang), order='F')
     yy2 = y[:, 1].reshape((options.Ndist, options.Nang), order='F')
 
-    Y = np.asfortranarray(np.arange(1, options.Ndist * options.sampling + 1, options.sampling))
-    X = np.asfortranarray(np.arange(1, options.Nang + 1))
-    X_grid, Y_grid = np.meshgrid(X, Y)
+    x_vals = np.arange(1, options.Nang + 1)
+    y_vals = np.arange(1, options.Ndist * options.sampling + 1, options.sampling)
+    points = (y_vals, x_vals)
     
-    Yq = np.asfortranarray(np.arange(1, options.Ndist * options.sampling + 1))
-    Xq = np.asfortranarray(np.arange(1, options.Nang + 1))
-    Xq_grid, Yq_grid = np.meshgrid(Xq, Yq)
+    yq_vals = np.arange(1, options.Ndist * options.sampling + 1)
+    xq_vals = np.arange(1, options.Nang + 1)
+    Yq, Xq = np.meshgrid(yq_vals, xq_vals, indexing='ij')
+    query_points = np.stack([Yq.ravel(), Xq.ravel()], axis=-1)
     
     # Interpolation
     def interp_surface(data):
-        from scipy.interpolate import interp2d
-        f = interp2d(X, Y, data.astype(np.float64), kind=options.sampling_interpolation_method, bounds_error=False, fill_value=0.0)
-        return f(Xq, Yq)
+        from scipy.interpolate import interpn
+        return np.asfortranarray((interpn(points, data.astype(np.float64), query_points, method=options.sampling_interpolation_method, bounds_error=False).reshape(Yq.shape)))
     
     x1 = interp_surface(xx1)
     x2 = interp_surface(xx2)
@@ -43,7 +42,6 @@ def increaseSampling(options, x=None, y=None, interpolate_sinogram=False):
     x = np.asfortranarray(np.column_stack((x1.ravel('F'), x2.ravel('F'))))
     y = np.asfortranarray(np.column_stack((y1.ravel('F'), y2.ravel('F'))))
 
-    # Interpolate sinogram if needed
     if interpolate_sinogram:
         if options.SinM.ndim == 1:
             options.SinM = options.SinM.reshape((options.Ndist, options.Nang, -1), order='F')
@@ -59,21 +57,38 @@ def increaseSampling(options, x=None, y=None, interpolate_sinogram=False):
 
 
 def interpolateSinog(SinM, sampling, Ndist, Nang, sampling_interpolation_method):
+    from scipy.interpolate import interpn
     if SinM.ndim == 1 or SinM.shape[2] == 1:
         SinM = np.reshape(SinM, (Ndist, Nang, -1), order='F')
     if SinM.ndim == 3:
         SinM = np.reshape(SinM, (SinM.shape[0], SinM.shape[1], SinM.shape[2], 1), order='F')
-    X, Y = np.asfortranarray(np.meshgrid(np.arange(1, Nang + 1), np.arange(1, Ndist * sampling + 2, sampling)))
-    Xq, Yq = np.asfortranarray(np.meshgrid(np.arange(1, Nang + 1), np.arange(1, Ndist * sampling + 2)))
+    
+    y = np.arange(1, Ndist * sampling + 1, sampling)
+    x = np.arange(1, Nang + 1)
+    grid = (np.asfortranarray(y), np.asfortranarray(x))
 
+    yq = np.arange(1, Ndist * sampling + 1)
+    xq = np.arange(1, Nang + 1)
+    Xq, Yq = np.meshgrid(xq, yq, indexing='xy')
+    Xq = Xq.T
+    Yq = Yq.T
+    query_points = np.asfortranarray(np.column_stack((Yq.ravel(), Xq.ravel())))
+    
     SinM_uus = np.zeros((SinM.shape[0] * sampling, Nang, SinM.shape[2], SinM.shape[3]), dtype=np.float32, order='F')
 
     for uu in range(SinM.shape[3]):
         for kk in range(SinM.shape[2]):
             sinogram_slice = SinM[:, :, kk, uu].astype(np.float64)
 
-            f_interp = interp2d(X[0, :], Y[:, 0], sinogram_slice, kind=sampling_interpolation_method)
-            interpolated = f_interp(Xq[0, :], Yq[:, 0])
-            SinM_uus[:, :, kk, uu] = interpolated[:-1, :]
+            interpolated = interpn(
+                grid,
+                sinogram_slice,
+                query_points,
+                method=sampling_interpolation_method,
+                bounds_error=False
+            )
+            interpolated = interpolated.reshape(Ndist * sampling, Nang, order='F')
 
-    return SinM_uus.astype(np.float32).reshape(-1)
+            SinM_uus[:, :, kk, uu] = np.asfortranarray(interpolated)
+
+    return SinM_uus.astype(np.float32).ravel('F')
